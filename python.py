12fb32132df9fcdf5351365453110fb53525e2d7
Gustavecbr <123983867+Gustavecbr@users.noreply.github.com>
2023-03-25 19:15:59 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,15 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 25 18:45:24 2023
+
+@author: gusta
+"""
+
+
 import pandas as pd
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import dcc
+from dash import html
 import plotly.graph_objs as go
 import dash.dependencies as dd
 Input = dd.Input
 Output = dd.Output
 # Charger les données
-df = pd.read_csv('/home/ec2-user/output.csv', header=None, names=['date', 'time', 'price'], delimiter=';', decimal='.')
-df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'], format='%d/%m/%Y %H:%M:%S')
+df = pd.read_csv('//wsl.localhost/Ubuntu/home/gustave/output.csv', header=None, names=['date', 'time', 'price'], delimiter=',', decimal='.')
+df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'], format= '%Y-%m-%d %H:%M:%S')
 df['price'] = df['price'].str.replace('$', '', regex=False).astype(float)
 df['price'] = df['price'].fillna(method='ffill')
 df["variation"] = df["price"].diff()
@@ -21,7 +28,7 @@ def get_color(variation):
         return "red"
     else :
         return "green"
-    
+
 # Calculer des statistiques
 volatility = round(ts.pct_change().std() * 100, 2)
 mean_price = '${:,.2f}'.format(round(ts.mean(), 2))
@@ -30,47 +37,47 @@ daily_return = round((ts.iloc[-1]-ts.iloc[0])/ts.iloc[0]*100, 2)
 
 # Initialiser l'application Dash
 app = dash.Dash(__name__)
+server = app.server
 
-app = dash.Dash(__name__)
 app.layout = html.Div(children=[
     html.Div(children=[
         html.Img(src='https://hpg.com.br/wp-content/uploads/2023/09/GateToken-Vale-A-Pena-2023-Onde-Comprar-1000x600.jpeg', style={'width': '20%'}),
-        html.H3(children='Presentation of the Gate Token',style={'text-align': 'center','font-weight': 'bold', 'font-size': '45px','margin-left': '40px','margin-right': '40px'}),
+        html.H3(children='Presentation of the Gate Token',style={'text-align': 'center','font-weight': 'bold', 'font-size': '45px','margin-left': '40px','margin-right': '40px'}),      
         html.Table(
             children=[
                 html.Tr(
                     children=[
-                        html.Td(children=' '),  
-                        html.Td(children='Performance'),                       
+                        html.Td(children=' '),
+                        html.Td(children='Performance'),
                     ]
                 ),
                 html.Tr(
                     children=[
                         html.Td(children='Daily return'),
-                        html.Td(children='{:.2f}%'.format(daily_return),style={'color': 'red' if daily_return < 0 else 'green'})
+                        html.Td(children='{:.2f}%'.format(daily_return),style={'color': 'red' if daily_return < 0 else 'green'},id='daily_return')
                     ]
                  ),
                 html.Tr(
                     children=[
                         html.Td(children='Volatility'),
-                        html.Td(children=f'{volatility}%', style={'color': 'red' if volatility > 10 else 'green'})
+                        html.Td(children=f'{volatility}%', style={'color': 'red' if volatility > 10 else 'green'},id='volatility')
                     ]
                 ),
                 html.Tr(
                     children=[
                         html.Td(children='Mean'),
-                        html.Td(children=mean_price)
+                        html.Td(children=mean_price,id='mean_price')
                     ]
                 ),
-                html.Tr(
-                    children=[
-                        html.Td(children='Actual Price'),
-                        html.Td(children=current_price)
-                    ]
-                )
-            ]
-        ),
-        
+            html.Tr(
+                children=[
+                    html.Td(children='Actual Price'),
+                    html.Td(children=current_price,id='actual_price')
+                ]
+            )
+        ]
+    ),
+
     ], style={'display': 'flex'}),
 
     html.Br(),
@@ -81,7 +88,7 @@ app.layout = html.Div(children=[
         end_date=ts.index.max().date(),
         min_date_allowed=ts.index.min().date(),
         max_date_allowed=ts.index.max().date()
-    ),
+        ),
     html.Br(),
     html.Br(),
     html.Br(),
@@ -92,44 +99,58 @@ app.layout = html.Div(children=[
                 {'x': ts.index, 'y': ts.values, 'type': 'line', 'name': 'Prix', 'line' : {'color': get_color(ts)} }
             ],
             'layout':  go.Layout(
-                title='Gate Token', 
-                plot_bgcolor='black', 
+                title='Gate Token',
+                plot_bgcolor='black',
                 paper_bgcolor='black',
                 font=dict(color='white'),
-                xaxis=dict(title='Time', titlefont=dict(color='white')), 
-                yaxis=dict(title='Price', titlefont=dict(color='white')), 
+                xaxis=dict(title='Time', titlefont=dict(color='white')),
+                yaxis=dict(title='Price', titlefont=dict(color='white')),
             )
             
         }
     ),
-    html.H3(children='Evolution of the Price of the Gate Token', style={'text-align': 'center'}),  
+    html.H3(children='Evolution of the Price of the Gate Token', style={'text-align': 'center'}),
 ],style = {'border': '1px solid black', 'width': '90%', 'margin-left': 'auto', 'margin-right': 'auto', 'text-align': 'right', 'background-color': 'black', 'color': 'white', 'padding': '10px', 'height': '1000vh'}
 )
 
 # Mettre à jour le graphique en fonction de la plage de dates sélectionnée
 @app.callback(
     Output('graph', 'figure'),
+    Output('daily_return', 'children'),
+    Output('volatility', 'children'),
+    Output('mean_price', 'children'),
+    Output('actual_price', 'children'),
     Input('date-picker-range', 'start_date'),
     Input('date-picker-range', 'end_date')
 )
 
 def update_graph(start_date, end_date):
     filtered_ts = ts.loc[start_date:end_date]
+    # Calculer des statistiques pour les données filtrées
+    volatility = round(filtered_ts.pct_change().std() * 100, 2)
+    mean_price = '${:,.2f}'.format(round(filtered_ts.mean(), 2))
+    daily_return = round((filtered_ts.iloc[-1] - filtered_ts.iloc[0]) / filtered_ts.iloc[0] * 100, 2)
+    current_price = '${:,.2f}'.format(round(filtered_ts.iloc[-1], 2))
+
     return {
         'data': [
             {'x': filtered_ts.index, 'y': filtered_ts.values, 'type': 'line', 'name': 'Prix', 'line' : {'color': get_color(ts)} }
         ],
         'layout': go.Layout(
-            title='Gate Token', 
-            plot_bgcolor='black', 
+            title='Gate Token',
+            plot_bgcolor='black',
             paper_bgcolor='black',
             font=dict(color='white'),
-            xaxis=dict(title='Time', titlefont=dict(color='white')), 
-            yaxis=dict(title='Price', titlefont=dict(color='white')),      
-        )
-            
+            xaxis=dict(title='Time', titlefont=dict(color='white')),
+            yaxis=dict(title='Price', titlefont=dict(color='white')),
+        ),
+        'volatility': volatility,
+        'mean_price': mean_price,
+        'daily_return': daily_return,
+        'current_price': current_price
+        
+
     }
 
-
-if __name__ == '__main__':
-    app.run_server(host='54.88.50.122',debug=True)
+if __name__ == "__main__":
+        app.run_server(host="0.0.0.0",port=8050)
